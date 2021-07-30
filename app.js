3b82a00f0b511b677fb6c6c912e94c564901e764
aldiiskandar4@gmail.com <Mh2gm88a--FfqMnUUKFZ>
2021-07-30 14:24:47 +0700

    add: nodemon

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Hello World'));
 
-app.listen(3000, () => console.log('listenig on localhost:3000'));
+app.listen(3000, () => console.log('listenig on http://localhost:3000/'));
