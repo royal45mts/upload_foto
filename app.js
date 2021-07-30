d4b262e1593b80e5931da1936cf0bd422052805b
aldiiskandar4@gmail.com <Mh2gm88a--FfqMnUUKFZ>
2021-07-30 18:48:07 +0700

    add: production

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyparse = require('body-parser')
+const compression = require('compression')
 const app = express();
 app.use(bodyparse.json())
+app.use(compression())
+
 require('./routes/index')(app)
 app.listen(3000, () => console.log('listenig on http://localhost:3000/'));
