9f192ef9f87c180e59b7f0aab06667e9d19677e6
aldiiskandar4@gmail.com <Mh2gm88a--FfqMnUUKFZ>
2021-07-30 18:50:55 +0700

    add: env

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const compression = require('compression')
 const app = express();
 app.use(bodyparse.json())
 app.use(compression())
+require('dotenv').config()
 
 require('./routes/index')(app)
-app.listen(3000, () => console.log('listenig on http://localhost:3000/'));
+app.listen(process.env.PORT, () => console.log(`listenig on  http://localhost:${process.env.PORT}/`));
